d631d84d7bdbc5e66d2ece0113c80d7ad48ec213
Rakhi-kashyap <101251859+Rakhikahyap@users.noreply.github.com>
2022-07-20 23:13:45 +0530

    Add files via upload

---
+++ eight.js
@@ -0,0 +1,13 @@
+var time =11;
+if (time >=5 && time<=6){
+    console.log("sleep well");
+}
+else if (time >=6 && time <=7){
+    console.log("morning exercise")
+}
+else if (time >7 && time <8.30){
+    console.log("breakfast");
+}
+else if (time > 8.40 && time <12){
+    console.log("study");
+}
