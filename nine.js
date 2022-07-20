3b7b66353bf36329f9ba82bca2ba827e9b20c0e3
Rakhi-kashyap <101251859+Rakhikahyap@users.noreply.github.com>
2022-07-20 23:23:28 +0530

    Add files via upload

---
+++ nine.js
@@ -0,0 +1,19 @@
+// let num1=200-125;
+// let readlineSync =require("readline-sync");
+// let num2=readlineSync.question("enter a number");
+// if (num1==num2){
+//     console.log("this is equal")
+// }
+// else{
+//     console.log("this is not equal")
+// }
+
+const num1=200-126;
+const readlineSync=require("readline-sync");
+const num2=readlineSync.question("enter a number");
+if (num1==num2){
+    console.log("this is equal")
+}
+else{
+    console.log("this is not equal")
+}
