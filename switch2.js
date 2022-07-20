1a20410a3d647173c2b74caa55dfe33d95a9e169
Rakhi-kashyap <101251859+Rakhikahyap@users.noreply.github.com>
2022-07-20 23:04:55 +0530

    Add files via upload

---
+++ switch2.js
@@ -0,0 +1,12 @@
+
+var text;
+switch (new Date().getDay()) {
+  case 6:
+    console.log("Today is Saturday");
+    break;
+  case 0:
+    console.log("Today is Sunday")
+    break;
+  default:
+    console.log("Looking forward to the Weekend");
+}
